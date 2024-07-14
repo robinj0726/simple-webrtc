50df21d12c9b42147a311dafc5d09c9efae5e127
robinj730 <88188467+robinj730@users.noreply.github.com>
2024-07-14 15:24:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Typography, Input } from "antd";
+import "./App.css";
 
+const { Title, Paragraph } = Typography;
+const { TextArea } = Input;
 function App() {
+  const renderHelper = () => {
+    return (
+      <div className="wrapper">
+        <Input placeholder="User ID" style={{ width: 240, marginTop: 16 }} />
+        <Input
+          placeholder="Channel Name"
+          style={{ width: 240, marginTop: 16 }}
+        />
+        <Button style={{ width: 240, marginTop: 16 }} type="primary">
+          Call
+        </Button>
+        <Button danger style={{ width: 240, marginTop: 16 }} type="primary">
+          Hangup
+        </Button>
+      </div>
+    );
+  };
+
+  const renderTextarea = () => {
+    return (
+      <div className="wrapper">
+        <TextArea
+          style={{ width: 240, marginTop: 16 }}
+          placeholder="Send message"
+        />
+        <TextArea
+          style={{ width: 240, marginTop: 16 }}
+          placeholder="Receive message"
+          disabled
+        />
+        <Button
+          style={{ width: 240, marginTop: 16 }}
+          type="primary"
+          // disabled={sendButtonDisabled}
+        >
+          Send Message
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">
+        <Title>WebRTC</Title>
+        <Paragraph>
+          This is a simple demo app that demonstrates how to build a WebRTC
+          application from scratch, including a signaling server. It serves as a
+          step-by-step guide to help you understand the process of implementing
+          WebRTC in your own projects.
+        </Paragraph>
+        <div
+          className="wrapper-row"
+          style={{ justifyContent: "space-evenly", width: "50%" }}
         >
-          Learn React
-        </a>
-      </header>
+          {renderHelper()}
+          {renderTextarea()}
+        </div>
+        <div className="playerContainer" id="playerContainer">
+          <video id="peerPlayer" autoPlay style={{ width: 640, height: 480 }} />
+          <video
+            id="localPlayer"
+            autoPlay
+            style={{ width: 640, height: 480 }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
